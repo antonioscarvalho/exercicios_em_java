d926dd6542196fa199ae2a8a49a8c691f2d65172
antonioscarvalho <antoniosousac2003@gmail.com>
2024-03-26 21:37:17 -0300

    new_exercises

---
+++ Argumentos.java
@@ -0,0 +1,8 @@
+public class Argumentos {
+    public static void main(String[] args) {
+        System.out.println("Você digitou: " + args[1]);
+    }
+}
+
+// Para compilar escreva javac Argumentos.java //
+// Para rodar, digite java Argumentos blábláblá //
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
-/**
- * exercise_1
- */
-public class exercise_1 {
+public class HelloWorld {
 
     public static void main(String[] arguments){
         System.out.println("Hello, World!");
     }
 }
+
+//Para compilar escreva javac HelloWorld.java//
